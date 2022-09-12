089abf6693bd7b8c0352a0f30d63b7f4f03f2557
NitroTech 4K <crtraer@gmail.com>
2022-09-12 12:32:22 -0300

    Test 1

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 2!");
+            Console.ReadKey(true);
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.Beep(800, 100);
+                Console.WriteLine("FEESH?.exe has run into a critical error and has stopped working properly. Please restart your computer, and try again");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("ERROR CODE: POISSON_FAT32_DRIVER_ERROR");
+                Console.WriteLine("");
+                Console.WriteLine("Please press Alt + F4 to stop the program");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
